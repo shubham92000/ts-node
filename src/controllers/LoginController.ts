4fe0effde6cc68632329146fb563c1b9d335bf32
shubham92000 <sbhsingh616@gmail.com>
2022-08-17 09:54:25 +0530

    refactor

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,6 +1,5 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import { controller } from './decorators/controller';
-import { get } from './decorators/routes';
+import { Request, Response } from 'express';
+import { get, controller } from './decorators';
 
 @controller('/auth')
 class LoginController {
