5bdf769a9bbc7c08a331cd71d75a2ff26c457446
shubham92000 <sbhsingh616@gmail.com>
2022-08-17 07:05:45 +0530

    get decorator

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['asdf'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
